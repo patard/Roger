cef41f6c4b311124685546d41db8338753ca3218
Patrice Paparelli <patrice.paparelli@thalesgroup.com>
2016-08-04 10:58:37 +0200

    maj des constantes des PID
    Signed-off-by: Patrice Paparelli <patrice.paparelli@thalesgroup.com>

--- software/interface-rpi-arduino/python/robot.py
+++ software/interface-rpi-arduino/python/robot.py
@@ -23,18 +23,22 @@ wheel_diameter = 0.134 # meters
 wheel_distance = 0.28 # meters
 wheel_distance = 0.265 # meters
 
+MAX_MOTOR_CMD = 150 # commande max des moteurs
+MIN_MOTOR_CMD = 20  # commande min des moteurs
+
 # constante des PID distance et cap
 Kp_Dist = 200.
-Kp_Dist = 300.
 Ki_Dist = 20.
-Kd_Dist = 0.
-
-Kp_Cap = 75.
-Ki_Cap = 8.
-Kd_Cap = 2.
+Kd_Dist = 50.
 
 
+#Kp_Cap = 75.
+#Ki_Cap = 8.
+#Kd_Cap = 2.
 
+Kp_Cap = 200.
+Ki_Cap = 20.
+Kd_Cap = 10.
 
 #################################################
 def init() :
@@ -60,7 +64,7 @@ def backward_left() :
 #################################################
 
 
-MAX_CMD = 100
+
 #################################################
 def backward_right() :
   interface_i2c.digitalWrite(wheel_direction_pin_right, 0);
@@ -68,18 +72,18 @@ def backward_right() :
 
 #################################################
 def set_cmd_motor_left(motor_cmd) :
-	if ( motor_cmd > MAX_CMD ) :
-		motor_cmd = MAX_CMD
-	if ( motor_cmd <= 25 ) :
+	if ( motor_cmd > MAX_MOTOR_CMD ) :
+		motor_cmd = MAX_MOTOR_CMD
+	if ( motor_cmd <= MIN_MOTOR_CMD ) :
 		motor_cmd = 0	
 	interface_i2c.analogWrite(PWM_pin_left, int(motor_cmd));	
 #################################################
 
 #################################################
 def set_cmd_motor_right(motor_cmd) :
-	if ( motor_cmd > MAX_CMD ) :
-		motor_cmd = MAX_CMD
-	if ( motor_cmd <= 25 ) :
+	if ( motor_cmd > MAX_MOTOR_CMD ) :
+		motor_cmd = MAX_MOTOR_CMD
+	if ( motor_cmd <= MIN_MOTOR_CMD ) :
 		motor_cmd = 0
 	interface_i2c.analogWrite(PWM_pin_right, int(motor_cmd));	
 #################################################
